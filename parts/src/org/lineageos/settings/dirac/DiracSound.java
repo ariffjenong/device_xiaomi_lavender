9979875ab3b99b667c530f6a5436c49d0833723b
TheScarastic <warabhishek@gmail.com>
2019-10-04 12:20:49 +0530

    lavender: Dont restore values on boot
    * Dirac libs are smart enough to store these values on power-off
      and restore them on boot.
    
    Change-Id: I6c1eea0afc631d6e436f466f7b84d26c56cf6225
    Signed-off-by: Coptan99 <omarcoptan9@gmail.com>

--- parts/src/org/lineageos/settings/dirac/DiracSound.java
+++ parts/src/org/lineageos/settings/dirac/DiracSound.java
@@ -46,13 +46,6 @@ public class DiracSound extends AudioEffect {
         checkStatus(setParameter(DIRACSOUND_PARAM_MUSIC, enable));
     }
 
-    public int getHeadsetType() throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
-        int[] value = new int[1];
-        checkStatus(getParameter(DIRACSOUND_PARAM_HEADSET_TYPE, value));
-        return value[0];
-    }
-
     public void setHeadsetType(int type) throws IllegalStateException,
             IllegalArgumentException, UnsupportedOperationException {
         checkStatus(setParameter(DIRACSOUND_PARAM_HEADSET_TYPE, type));
@@ -62,15 +55,5 @@ public class DiracSound extends AudioEffect {
             IllegalArgumentException, UnsupportedOperationException {
         checkStatus(setParameter(new int[]{DIRACSOUND_PARAM_EQ_LEVEL, band},
                 String.valueOf(level).getBytes()));
-    }
-
-    public float getLevel(int band) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
-        int[] param = new int[2];
-        byte[] value = new byte[10];
-        param[0] = DIRACSOUND_PARAM_EQ_LEVEL;
-        param[1] = band;
-        checkStatus(getParameter(param, value));
-        return new Float(new String(value)).floatValue();
     }
 }
--- parts/src/org/lineageos/settings/dirac/DiracUtils.java
+++ parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -29,9 +29,6 @@ public final class DiracUtils {
             mInitialized = true;
             mContext = context;
             mDiracSound = new DiracSound(0, 0);
-            mDiracSound.setMusic(mDiracSound.getMusic());
-            mDiracSound.setHeadsetType(mDiracSound.getHeadsetType());
-            setLevel(getLevel());
         }
     }
 
@@ -41,16 +38,6 @@ public final class DiracUtils {
 
     protected static boolean isDiracEnabled() {
         return mDiracSound.getMusic() == 1;
-    }
-
-    protected static String getLevel() {
-        String selected = "";
-        for (int band = 0; band <= 6; band++) {
-            int temp = (int) mDiracSound.getLevel(band);
-            selected += String.valueOf(temp);
-            if (band != 6) selected += ",";
-        }
-        return selected;
     }
 
     protected static void setLevel(String preset) {
