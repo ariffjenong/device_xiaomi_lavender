f26231398453ea51c2cbbf71ebfa26a4de010daa
TH779 <tongsj2333@gmail.com>
2020-09-05 22:24:11 +0800

    lavender: Import XiaomiParts from Xiaomi sdm845-common
     * From https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/65f0dd1a73e2088ceeca6922adef90fde21420e9.
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: Idf661a96f770a70725c38e13edcc28ab379af2b6
    Signed-off-by: Coptan99 <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -358,6 +358,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
