fec9db6753b18688fff74cb7763961a0c1e71cfc
ariffjenong <arifbuditantodablekk@gmail.com>
2023-01-21 19:52:31 +0700

    lavender: Nusantara Bringup! Signed-off-by: ariffjenong <arifbuditantodablekk@gmail.com>

--- nad_lavender.mk
+++ nad_lavender.mk
@@ -12,15 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common Evolution X stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-EVO_BUILD_TYPE := OFFICIAL
+# Inherit some common Nusantara Project stuff
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+NAD_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_MINI_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_lavender
+PRODUCT_NAME := nad_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
