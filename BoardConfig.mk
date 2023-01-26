eb6259ac712911048beff682d4616fc956049bd0
ariffjenong <arifbuditantodablekk@gmail.com>
2023-01-26 15:00:15 +0700

    Revert "lavender: Disable LLVM_BINUTILS"
    This reverts commit 5ea7de8fadc851038ac41d9918a28cb5205d482b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,7 +96,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := lavender_defconfig
-TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 
 # Partitions
