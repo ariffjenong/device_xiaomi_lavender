0e918d5191882c334cb9b2bc4ad931fe978e0669
ariffjenong <arifbuditantodablekk@gmail.com>
2023-05-15 20:02:42 +0700

    Revert "lavender: Import libtinyxml from LA.UM.9.6.2.r1-04100-89xx.0"
    This reverts commit 9275c9410aad08d02e8de368de4a49ea7d165049.

--- device.mk
+++ device.mk
@@ -162,6 +162,7 @@ PRODUCT_PACKAGES += \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
     libdisplayconfig \
+    libtinyxml \
     memtrack.sdm660 \
     vendor.display.config@2.0 \
     vendor.display.config@2.0_vendor
